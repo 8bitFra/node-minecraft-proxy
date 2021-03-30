f8b0b4b73322498816e193e1aede5a30cdc7e592
8bitFra <sonosolofra@gmail.com>
2021-03-30 13:32:46 +0200

    plist command / private chat with prefix

--- src/Plugins/ChatCommands.js
+++ src/Plugins/ChatCommands.js
@@ -1,11 +1,28 @@
+const fs = require('fs');
+
 function handleCommands (client, proxy, localServerOptions, proxyOptions) {
+
+  let rawdata;
+  let ignored;
+
+  try 
+  {
+    rawdata = fs.readFileSync('./ignored.json');
+    ignored = JSON.parse(rawdata);
+  } catch (error) 
+  {
+    rawdata = undefined;
+    ignored = undefined;
+  }
+
   client.on('chat', (data, metadata) => {
     let split = data.message.split(' ')
-    if (split[0] === '/server') {
+    if (split[0] === '/server') 
+    {
       if (proxy.serverList[split[1]]) {
         proxy.setRemoteServer(client.id, split[1])
       } else {
-        const msg = {
+        let msg = {
           color: 'red',
           translate: 'chat.type.text',
           with:[{"text":"Syntax Error"}]
@@ -13,7 +30,50 @@ function handleCommands (client, proxy, localServerOptions, proxyOptions) {
         client.write('chat', { message: JSON.stringify(msg), position: 0, sender: 0 })
       }
     }
+    else if (split[0] === '/plist')
+    {
+      let msg = "Player connessi al Proxy: " + proxy.playerCount;
+      client.write('chat', { message: JSON.stringify(msg), position: 0, sender: 0 })
+    }
+
+    if(ignoredcheck(data.message,ignored)!=undefined)
+    {
+      dispatchMessage(proxy.clients,client, ignoredcheck(data.message,ignored));
+    } 
+
   })
+}
+
+function ignoredcheck(message, obj)
+{
+  if(message!=undefined && obj!=undefined)
+  {
+    for (ele in obj.prefix)
+    {
+      if(message.substring(0,obj.prefix[ele].length)==obj.prefix[ele])
+      {
+        return message.substring(obj.prefix[ele].length,message.length);
+      }
+        
+    }
+  }
+  return undefined;
+}
+
+function dispatchMessage(clients, sender, message)
+{
+  let out = {
+    color: 'green',
+    translate: 'chat.type.text',
+    with:[{"text":"PROXY - "+sender.username},{"text":message}]
+  }
+
+  let print = JSON.stringify(out);
+
+  for(ele in clients)
+  {
+    clients[ele].write('chat', { message: print, position: 0, sender: sender.uuid });
+  }
 }
 
 module.exports = handleCommands
